62616906256c0807ab20f1bb39d6ce03266fae63
David Cordes <david.cordes@outlook.de>
2023-03-27 11:51:08 +0200

    -

--- ServerManagerBackend/src/de/david/serverManagerBackend/handler/ClientConnectionsHandler.java
+++ ServerManagerBackend/src/de/david/serverManagerBackend/handler/ClientConnectionsHandler.java
@@ -1,20 +1,43 @@
 package de.david.serverManagerBackend.handler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ClientConnectionsHandler {
 
     public static void startSocket(){
 
         try {
-            ServerSocket serverSocket = new ServerSocket();
+            ServerSocket serverSocket = new ServerSocket(25500);
+            waitForNewConnection(serverSocket);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
+    public static void waitForNewConnection(ServerSocket serverSocket){
+        Runnable rn = () -> {
+            try {
+                Socket s = serverSocket.accept();
+                System.out.println("Client connected");
+                //s.
+                InputStreamReader in = new InputStreamReader(s.getInputStream());
+                BufferedReader rd = new BufferedReader(in);
+
+                while (rd.ready()){
+
+                }
+
+                System.out.println(rd.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        Thread newClientConnection = new Thread(rn);
+        newClientConnection.start();
+    }
 
 }
--- ServerManagerSpigot/src/main/java/de/david/servermanagerspigot/Main.java
+++ ServerManagerSpigot/src/main/java/de/david/servermanagerspigot/Main.java
@@ -2,12 +2,27 @@ package de.david.servermanagerspigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public final class Main extends JavaPlugin {
+
+    private static Socket clientSocket;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        try {
+            System.out.println("connecting to serverManger");
+            clientSocket = new Socket("localhost", 255);
+            System.out.println("connected");
+            PrintWriter pr = new PrintWriter(clientSocket.getOutputStream());
+            pr.println("server online");
+            pr.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
